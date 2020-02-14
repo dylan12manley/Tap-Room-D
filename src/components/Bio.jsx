63adc71eb9dfffccfe5d18a2648e4c4aa8231d26
Dylan Manley <dylan.manley@gmail.com>
2020-02-14 13:33:26 -0800

    add background image and styling to bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -5,7 +5,7 @@ import "../styles/Bio.css"
 function Bio(){
   return (
   <>
-  <div>
+  <div className="bioBody">
     <h3>Tap Room D</h3>
     <h4>Established 2020</h4>
     <p>Tap room D is a new tap room in SW Portland. We offer the finest choices of local beers, without all the extra hoppy mess that usually comes along with NW style beers.</p>
