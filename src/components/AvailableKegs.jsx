a315111682e560e15e4eaab92e7567f3609d1fe5
Dylan Manley <dylan.manley@gmail.com>
2020-02-14 10:03:37 -0800

    fill in tap list contents

--- src/components/AvailableKegs.jsx
+++ src/components/AvailableKegs.jsx
@@ -12,6 +12,30 @@ const kegsAvailable = [
     brand: "Pelican",
     price: "5.50",
     abv: "5.4"
+  },
+  {
+    name: "Hazelnut Brown Nectar",
+    brand: "Rouge",
+    price: "6.00",
+    abv: "5.6"
+  },
+  {
+    name: "Dreamland",
+    brand: "Rouge",
+    price: "6.00",
+    abv: "4.8"
+  },
+  {
+    name: "Double Time Cherry Lime",
+    brand: "Rouge",
+    price: "7.50",
+    abv: "9.1"
+  },
+  {
+    name: "Blask Butte Porter",
+    brand: "Deschutes",
+    price: "5.50",
+    abv: "5.2"
   }
 ]
 function AvailableKegs (){
